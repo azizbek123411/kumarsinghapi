bf1eb602dfc8046e5b757333910e451324b23a3d
hp <sultonovazizbek73@gmail.com>
2024-08-18 21:00:29 +0500

    models

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:kumarsinghapi/model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,16 +12,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List users = [];
+  List<Model> users = [];
 
-  Future<void> fetchUsers() async {
+  void fetchUsers() async {
     const url = 'https://randomuser.me/api/?results=100';
     final uri = Uri.parse(url);
     final response = await http.get(uri);
     final body = response.body;
     final json = jsonDecode(body);
+    final result = json['results'] as List<dynamic>;
+    final transformed = result.map((e) {
+      return Model(
+        email: e['email']??"hello",
+        country: e['country']??"hello",
+        image: e['picture']['medium']??"hello",
+      );
+    }).toList();
     setState(() {
-      users = json['results'];
+      users=transformed;
     });
   }
 
@@ -30,30 +39,31 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Random users'),
       ),
-      body: FutureBuilder(
-        future: fetchUsers(),
-        builder: (context, snapshot) {
-          return ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (context, index) {
-                final user = users[index];
-                return ListTile(
-                  title: Text(
-                    "${user['name']['first']} ${user['name']['last']}",
-                    style: const TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                  leading: CircleAvatar(
-                    child: Image.network(
-                      user['picture']['medium'],
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                );
-              });
-        },
+      body:  ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user = users[index];
+            return ListTile(
+              subtitle: Text(user.country??""),
+              title: Text(
+                user.email,
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              leading: CircleAvatar(
+                child: Image.network(
+                  user.image,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: fetchUsers,
       ),
+
     );
   }
 }
---
+++ lib/model.dart
@@ -0,0 +1,12 @@
+class Model {
+  final String email;
+  final String image;
+  final String country;
+
+  Model({
+    required this.email,
+    required this.country,
+    required this.image,
+  });
+
+}
