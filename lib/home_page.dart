dbbd37c451c70fe89d664169f2f464f8bb5143df
hp <sultonovazizbek73@gmail.com>
2024-08-18 17:35:15 +0500

    fetched

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:kumarsinghapi/api_service.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,30 +11,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List users = [];
+
+  Future<void> fetchUsers() async {
+    const url = 'https://randomuser.me/api/?results=100';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Random users'),
+        title: const Text('Random users'),
       ),
       body: FutureBuilder(
-        future: ApiService.getData(),
-        builder: (BuildContext context, snapshot) {
-          if(snapshot.connectionState==ConnectionState.waiting){
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }else if(snapshot.hasError){
-            return Center(child: Text(snapshot.error.toString()));
-          }else{
-            return ListView.builder(
-                itemCount: 6,
-                itemBuilder: (context,index){
-              return ListTile(
-                title: Text(snapshot.stackTrace.toString()),
-              );
-            });
-          }
+        future: fetchUsers(),
+        builder: (context, snapshot) {
+          return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index];
+                return ListTile(
+                  title: Text(
+                    "${user['name']['first']} ${user['name']['last']}",
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  leading: CircleAvatar(
+                    child: Image.network(
+                      user['picture']['medium'],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              });
         },
       ),
     );
